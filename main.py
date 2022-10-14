a90e7f75dbb1021bd87d06d9a52496cd9fd714a6
CodeWracker <dev.rodrigofs@gmail.com>
2022-10-14 11:38:31 -0300

    resultados com TP.... etc

--- main.py
+++ main.py
@@ -10,20 +10,57 @@ from numpy.core.numeric import NaN
 from pprint import pprint
 from tqdm import tqdm
 
-
+color_real = [0, 0, 255] # color: BGR
+color_calc = [255, 0, 0] # color: BGR
+def calc_intersection_of_circles(center_real,radius_real,center_calc,radius_calc,im_shape):
+    img1 = np.zeros(im_shape, np.uint8)
+    center_real = (int(center_real[0]),int(center_real[1]))
+    center_calc = (int(center_calc[0]),int(center_calc[1]))
+    cv2.circle(img1, center_real, int(radius_real), color_real, -1)
+    img2 = np.zeros(im_shape, np.uint8)
+    cv2.circle(img2, center_calc, int(radius_calc), color_calc, -1)
+    return img1 + img2
 
 
 def calc_dist2d(p1, p2):
     return math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
 
 
-def calc_error(p_calc, p_real, iris_dt):
-    pcalc_center, pcalc_radius = p_calc
-    preal_center, preal_radius = p_real
+def calc_error(img_validation):
+    pixels = np.array(img_validation)
+    pixels = pixels.reshape(-1, 3)
+    pixels = pixels.tolist()
+    #print((np.array(color_calc) + np.array(color_real)).tolist())
+    true_negative = []
+    true_positive = []
+    false_negative = []
+    false_positive = []
+    for p in pixels:
+        #print(p)
+        
+        if p == [0, 0, 0]:
+            true_negative.append(p)
+        elif p == color_real:
+            false_negative.append(p)
+        elif p == color_calc:
+            false_positive.append(p)
+        elif p == (np.array(color_calc) + np.array(color_real)).tolist():
+            true_positive.append(p)
+    
 
     data = {
-        "center_error": calc_dist2d(pcalc_center, preal_center),
-        "radius_error": abs(pcalc_radius - preal_radius)
+        "TP":            len(true_positive),
+        "FP":            len(false_positive),
+        "TN":            len(true_negative),
+        "FN":            len(false_negative),
+        "TPR":           len(true_positive)/(len(true_positive)+len(false_negative)),
+        "FPR":           len(false_positive)/(len(false_positive)+len(true_negative)),
+        "TNR":           len(true_negative)/(len(false_positive)+len(true_negative)),
+        "FNR":           len(false_negative)/(len(true_positive)+len(false_negative)),
+        "ACC":          (len(true_positive)+len(true_negative))/(len(true_positive)+len(true_negative)+len(false_positive)+len(false_negative)),
+        "sensitivity":   len(true_positive)/(len(true_positive)+len(false_negative)),
+        "specificity":   len(true_negative)/(len(false_positive)+len(true_negative)),
+        "weighted_acc":  (len(true_positive)/(len(true_positive)+len(false_negative)) + len(true_negative)/(len(false_positive)+len(true_negative)))/2,
     }
     return data
 def get_eye_data_from_pickle(pickle_file):
@@ -48,6 +85,9 @@ def get_eye_data_from_pickle(pickle_file):
         pupil_dt[0], pupil_dt[2])/2 + calc_dist2d(pupil_dt[1], pupil_dt[3])/2)/2
     return iris_dt,(pupil_dt,p_real_center,p_real_radius)
 
+fourcc = cv2.VideoWriter_fourcc(*'XVID')  
+out = cv2.VideoWriter('output.avi',fourcc, 2, (240,80))
+
 if __name__ == "__main__":
     print("Iniciando testes")
     df_results = pd.DataFrame()
@@ -56,11 +96,22 @@ if __name__ == "__main__":
     # carrega os dados de teste
     dataset = datasets[0]
     file_names = []
-    center_errors = []
-    radius_errors = []
+    TPs = []
+    FPs = []
+    TNs = []
+    FNs = []
+    TPRs = []
+    FPRs = []
+    TNRs = []
+    FNRs = []
+    ACCs = []
+    sensitivities = []
+    specificities = []
+    weights_acc = []
     real_x = [] # p_real[1]
     real_y = [] # p_real[0]
     real_radius = []
+    frames = 0
     test_data_folders = os.listdir(f"datasets/{dataset}")
     for folder in tqdm(test_data_folders):
         files = os.listdir(f"datasets/{dataset}/{folder}")
@@ -74,27 +125,52 @@ if __name__ == "__main__":
                 iris_dt,(pupil_dt,p_real_center,p_real_radius) = get_eye_data_from_pickle(pickle_file)
                 # calcula o resultado
                 pcalc_center, pcalc_radius = model.detect_pupil(img, iris_dt)
-                cv2.circle(img, (int(pcalc_center[0]),int(pcalc_center[1])), int(pcalc_radius), (0, 255, 0), 1)
-                cv2.circle(img, (int(p_real_center[0]),int(p_real_center[1])), int(p_real_radius), (0, 0, 255), 1)
+                cv2.circle(img, (int(pcalc_center[0]),int(pcalc_center[1])), int(pcalc_radius),  color_calc, 1)
+                cv2.circle(img, (int(p_real_center[0]),int(p_real_center[1])), int(p_real_radius), color_real, 1)
                 # calcula o erro
-                error = calc_error((pcalc_center, pcalc_radius),(p_real_center,p_real_radius),iris_dt)
+                img_validation = calc_intersection_of_circles(p_real_center,p_real_radius,pcalc_center,pcalc_radius,img.shape)
+                error = calc_error((img_validation))
+                numpy_horizontal_concat = np.concatenate((img, img_validation), axis=1)
                 # salva os resultados
                 file_names.append(file.split('.png')[0])
-                center_errors.append(error['center_error'])
-                radius_errors.append(error['radius_error'])
+                TPs.append(error["TP"])
+                FPs.append(error["FP"])
+                TNs.append(error["TN"])
+                FNs.append(error["FN"])
+                TPRs.append(error["TPR"])
+                FPRs.append(error["FPR"])
+                TNRs.append(error["TNR"])
+                FNRs.append(error["FNR"])
+                ACCs.append(error["ACC"])
+                sensitivities.append(error["sensitivity"])
+                specificities.append(error["specificity"])
+                weights_acc.append(error["weighted_acc"])
                 real_x.append(p_real_center[1])
                 real_y.append(p_real_center[0])
                 real_radius.append(p_real_radius)
-                pprint(error)
+                #pprint(error)
                 #print("")
 
-                cv2.imshow("img",img)
-                cv2.waitKey(0)
-                cv2.destroyAllWindows()
+                #cv2.imshow("img",numpy_horizontal_concat)
+                if(frames<400):
+                    out.write(numpy_horizontal_concat)
+                frames+=1
+                #cv2.waitKey(0)
+                #cv2.destroyAllWindows()
         os.system("cls")
     df_results['file_name'] = np.array(file_names)
-    df_results['center_error'] = np.array(center_errors)
-    df_results['radius_error'] = np.array(radius_errors)
+    df_results['TP'] = np.array(TPs)
+    df_results['FP'] = np.array(FPs)
+    df_results['TN'] = np.array(TNs)
+    df_results['FN'] = np.array(FNs)
+    df_results['TPR'] = np.array(TPRs)
+    df_results['FPR'] = np.array(FPRs)
+    df_results['TNR'] = np.array(TNRs)
+    df_results['FNR'] = np.array(FNRs)
+    df_results['ACC'] = np.array(ACCs)
+    df_results['sensitivity'] = np.array(sensitivities)
+    df_results['specificity'] = np.array(specificities)
+    df_results['weighted_acc'] = np.array(weights_acc)
     df_results['real_x'] = np.array(real_x)
     df_results['real_y'] = np.array(real_y)
     df_results['real_radius'] = np.array(real_radius)
--- results/analyse_result.py
+++ results/analyse_result.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+from tqdm import tqdm
 import matplotlib.pyplot as plt
 if __name__ == "__main__":
     results = os.listdir("results")
@@ -14,9 +15,8 @@ if __name__ == "__main__":
     df_margins = pd.read_csv('results/error_margins/error_margins.csv', index_col=0)
     df_margins = df_margins[df_margins['dataset'] == csv_results[analyse_index].split('.')[0]]
 
-    # count values between margins
     
-    # draw histogram
+    '''# draw histogram
     fig, axs = plt.subplots(1, 1, sharey=True, tight_layout=True)
     axs.hist(df['center_error'], bins=50)
     axs.set_title('Center Error')
@@ -39,8 +39,17 @@ if __name__ == "__main__":
     print(f"Center Error under error margin of {df_margins['e_m_center'][0]}: {center_error} ({100*center_error/total:.2f}%)")
     radius_error = df[(df['radius_error'] <= df_margins['e_m_r'][0])]["radius_error"].count()
     print(f"Radius Error under error margin of {df_margins['e_m_r'][0]}: {radius_error} ({100*radius_error/total:.2f}%)")
-    print(f'Total number of results: {total}')
-    print(df.describe())
+    print(f'Total number of results: {total}')'''
+
+    columns = ['TP','FP','TN','FN','TPR','FPR','TNR','FNR','ACC','sensitivity','specificity','weighted_acc']
+    for column in tqdm(columns):
+        fig, axs = plt.subplots(1, 1, sharey=True, tight_layout=True)
+        axs.hist(df[column], bins=50)
+        axs.set_title(column)
+        fig.savefig(f"results/{csv_results[analyse_index].split('.')[0]}_{column}.png")
+
+    desc = df.describe()
+    desc.to_csv(f"results/descriptions/{csv_results[analyse_index].split('.')[0]}_desc.csv")
 
     #print(df_margins)
 
